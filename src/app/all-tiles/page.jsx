8888da0f844466488b0155b0e2b2441b806afbb5
Bushra <mafiaqueen734@gmail.com>
2026-05-03 23:52:39 +0600

    Debugged

--- src/app/all-tiles/page.jsx
+++ src/app/all-tiles/page.jsx
@@ -54,7 +54,7 @@ export default function AllTilesPage() {
         </div>
       </div>
 
-     //Grid 
+   
       <div className="max-w-7xl mx-auto px-4 py-10">
         {loading ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
